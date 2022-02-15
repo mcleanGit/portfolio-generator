0efb6316ce91f1319a97a79df6285242e2b00620
Don McLean <mclean@utoronto.ca>
2022-02-15 13:31:43 -0500

    app.js work file

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+const printProfileData = profileDataArr => {
+ for (let i = 0; i < profileDataArr.length; i += 1) {
+  console.log(profileDataArr[i]);
+ }
 
+ console.log('===============');
+
+ profileDataArr.forEach((profileItem) => {
+  console.log(profileItem)
+ });
+};
