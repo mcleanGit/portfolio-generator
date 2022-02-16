4c23b18c6b10622cd68e1be7e4b449eed441da7b
Don McLean <mclean@utoronto.ca>
2022-02-16 14:55:08 -0500

    app.js mod

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-const generatePage = (Name, github) => {
- return `
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [Name, github] = profileDataArgs;
+
+console.log(Name, github);
+console.log(generatePage(Name, github));
+
+const generatePage = (userName, githubName) => {
+  return `
  <!DOCTYPE html> 
  <html lang="en"> 
  <head>
@@ -8,7 +14,6 @@ const generatePage = (Name, github) => {
    <meta http-equiv="X-UA-Compatible" content="ie=edge">
    <title>Portfolio Demo</title>
  </head>
-
  <body>
    <h1>${Name}</h1>
    <h2><a href="https://github.com/${github}">Github</a></h2>
