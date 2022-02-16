58ff20668158b70d42d9700554fef3295d18f861
Don McLean <mclean@utoronto.ca>
2022-02-16 13:57:06 -0500

    update app.js re module work

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-/* const profileDataArgs = process.argv.slice(2);
+const generatePage = (Name, github) => {
+ return `
+ <!DOCTYPE html> 
+ <html lang="en"> 
+ <head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Portfolio Demo</title>
+ </head>
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
+ <body>
+   <h1>${Name}</h1>
+   <h2><a href="https://github.com/${github}">Github</a></h2>
+ </body>
+ </html>
+ `;
 };
-
-printProfileData(profileDataArgs);
-*/
-const generatePage = () => 'Name: Don, Github: mcleanGit';
