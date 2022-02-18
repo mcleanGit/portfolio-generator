b0f6a124199d6aa86de2ccae815be220985bcea2
Don McLean <mclean@utoronto.ca>
2022-02-18 15:25:04 -0500

    app.js update main

--- app.js
+++ app.js
@@ -5,12 +5,28 @@ const promptUser = () => {
   {
    type: 'input',
    name: 'name',
-   message: 'What is your name?'
-  },
+   message: 'What is your name? (Required)',
+   validate: nameInput => {
+     if (nameInput) {
+       return true;
+     } else {
+       console.log('Please enter your name!');
+       return false;
+     }
+     }
+   },
   {
    type: 'input',
    name: 'github',
-   message: 'Enter you GitHub UserName'
+   message: 'Enter you GitHub UserName (Required)',
+   validate: githubInput => {
+    if (githubInput) {
+      return true;
+    } else {
+      console.log('Please enter your name!');
+      return false;
+    }
+    }
   },
   {
    type: 'input',
@@ -33,13 +49,29 @@ const promptProject = portfolioData => {
  return inquirer.prompt([
   {
    type: 'input',
-   name: 'description',
-   message: 'What is the name of your project?'
+   name: 'name',
+   message: 'What is the name of your project? (Required)',
+   validate: projectNameInput => {
+    if (projectNameInput) {
+      return true;
+    } else {
+      console.log('Please enter your project name!');
+      return false;
+    }
+    }
   },
   {
    type: 'input',
    name: 'description',
-   message: 'Provide a description of the project (Required)'
+   message: 'Provide a description of the project (Required)',
+   validate: projectDescriptionInput => {
+    if (projectDescriptionInput) {
+      return true;
+    } else {
+      console.log('Please enter your project description!');
+      return false;
+    }
+    }
   },
   {
    type: 'checkbox',
@@ -50,7 +82,15 @@ const promptProject = portfolioData => {
   {
    type: 'input',
    name: 'link',
-   message: 'Enter the GitHub link to your project. (Required)'
+   message: 'Enter the GitHub link to your project. (Required)',
+   validate: linkInput => {
+    if (linkInput) {
+      return true;
+    } else {
+      console.log('Please enter your GitHub link for the project!');
+      return false;
+    }
+  }
   },
   {
    type: 'confirm',
